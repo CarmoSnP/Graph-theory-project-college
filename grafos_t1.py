6b3ffd8c6ac41dd75febe06641b50d7e5e1bfa5d
Igor Carmo <igorfarol03@gmail.com>
2026-04-10 11:38:45 -0300

    feat: add news methods class

--- grafos_t1.py
+++ grafos_t1.py
@@ -1,5 +1,15 @@
 import networkx as nx  # manipular grafos
 import matplotlib.pyplot as plt  # desenhar grafos
+import heapq  # da prioriadade para o algoritmo de prim
+from collections import deque  # fila de Busca em largura
+
+"""
+estrutura de variaveis
+peso = 2
+v    = 'A'   (origem)
+w    = 'C'   (destino)
+aid  = 'a2'  (identificador)
+"""
 
 
 class Grafo:
@@ -126,7 +136,7 @@ class Grafo:
         tipo = "Arco" if self.dirigido else "Aresta"
         print(f" [-] {tipo} '{id_aresta}' ({direcao}) removida.")
 
-    def mostrar_grafo(self, titulo="Grafo"):
+    def mostrar_grafo(self, titulo="Grafo", destaque_arestas=None):
         """
         exibe o grafo na tela usando o mathplotlib
         """
@@ -151,11 +161,31 @@ class Grafo:
         plt.title(titulo, fontsize=14, fontweight="bold")
 
         # desenha os vertices
-        nx.draw_networkx_nodes(G, pos, node_color="steelblue", node_size=800)
+        nx.draw_networkx_nodes(G, pos, node_color="steelblue", node_size=1500)
+
+        # monstra o nome do vertice
+        nx.draw_networkx_labels(
+            G,
+            pos,
+            labels={v: v for v in self.vertices},
+            font_color="white",
+            font_size=11,
+            font_weight="bold",
+        )
+
+        # destaque as linhas para mostar o prim
+        cores = []
+        for v, w in G.edges():
+            if destaque_arestas and (
+                (v, w) in destaque_arestas or (w, v) in destaque_arestas
+            ):
+                cores.append("red")
+            else:
+                cores.append("gray")
 
         # desenha as linhas
         nx.draw_networkx_edges(
-            G, pos, edge_color="gray", width=2, arrows=self.dirigido, arrowsize=20
+            G, pos, edge_color=cores, width=2, arrows=self.dirigido, arrowsize=20
         )
 
         # desenha o id e peso das linhas
@@ -165,13 +195,126 @@ class Grafo:
         plt.tight_layout()  # ajusto espacamento
         plt.show()  # exibir na tela
 
+    def prim(self, inicio):
+        """
+        encontrar a arvore minima a partir de qualquer vertice
+        """
+
+        if inicio not in self.vertices:
+            print(f"[!] Vertice '{inicio}' nao existe dentro do grafo.")
+            return
+
+        visitados = {inicio}
+        arestas_agm = []
+        custo_total = 0
+
+        # aqui estamos usando o heap para dentro de uma fila ele sempre retornar o menor elemento.
+        # formato usado {peso, vertice_origem, vertice_destino, id_aresta}
+        heap = []
+        for w, aid, peso in self.adj[inicio]:
+            heapq.heappush(heap, (peso, inicio, w, aid))
+
+        while heap and len(visitados) < len(self.vertices):
+            # aqui pegamos sempre a menor aresta do heap
+            peso, v, w, aid = heapq.heappop(heap)
+
+            # verificacao se foi visitado a aresta
+            if w in visitados:
+                continue
+
+            # adicona a lista de visitados
+            visitados.add(w)
+            arestas_agm.append((v, w, peso, aid))
+            custo_total += peso
+
+            # adiciona a aresta de w para o heap explorar
+            for vizinho, aid2, peso2 in self.adj[w]:
+                if vizinho not in visitados:
+                    heapq.heappush(heap, (peso2, w, vizinho, aid2))
+
+        return arestas_agm, custo_total
+
+    def mostrar_prim(self, inicio):
+        """
+        exibir o grafo de prim, arvore
+        """
+        arestas_agm, custo = self.prim(inicio)
+
+        print(f"\n Vertice inicial de prim '{inicio}'")
+        for v, w, peso, aid in arestas_agm:
+            print(f"{v} --[{aid}, peso={peso}]-->{w}")
+        print(f"custo total de prim: {custo}")
+
+        # destaque da arestas de prim
+        destaque = [(v, w) for (v, w, _, _) in arestas_agm]
+        self.mostrar_grafo(
+            titulo=f"AGM Prim (inicio: {inicio} | custo total: {custo})",
+            destaque_arestas=destaque,
+        )
+
+    def bsf(self, origem, destino):
+        """
+        busca em largura, achar o menor caminho entre 2 pontos
+        """
+        if origem not in self.vertices or destino not in self.vertices:
+            print(f"[!] Vertice de incio ou fim invalido.")
+            return [], []
+
+        fila = deque([origem])
+        visitados = {origem}
+        pai = {origem: None}  # guardar o pai de cada vertice
+        arestas_arvore = []
+
+        encontrado = False
+        while fila:
+            v = fila.popleft()  # fifo
+
+            if v == destino:
+                encontrado = True
+                break
+            for w, aid, _ in self.adj[v]:
+                if w not in visitados:
+                    visitados.add(w)
+                    pai[w] = v
+                    arestas_arvore.append((v, w))
+                    fila.append(w)
+        caminho = []
+        if encontrado:
+            cur = destino
+            while cur is not None:
+                caminho.append(cur)
+                cur = pai[cur]
+            caminho.reverse()  # inverte pois construimos de tras pra frente
+
+        return caminho, arestas_arvore
+
+    def mostrar_bfs(self, origem, destino):
+        """
+        executa BFS e exibe a arvore graficamente
+        usa fila, busca em largura
+        """
+        caminho, arestas_arvore = self.bsf(origem, destino)
+        print(f"\ Arvore de busca em largura: =='{origem}' --> '{destino}'==")
+        if caminho:
+            print(f"Caminho: {'->' .join(map(str, caminho))}")
+        else:
+            print("caminho nao encontrado")
+
+        self.mostrar_grafo(
+            titulo=f"BFS: {origem} -> {destino} | Caminho: {'->'.join(map(str, caminho))}",
+            destaque_arestas=arestas_arvore,
+        )
+
 
 # teste rápido
-# g = Grafo(dirigido=False)
-# g.insert_vertice("A")
-# g.insert_vertice("B")
-# g.insert_vertice("C")
-# g.insert_aresta("A", "B", peso=4)
-# g.insert_aresta("B", "C", peso=2)
-# g.insert_aresta("A", "C", peso=7)
-# g.mostrar_grafo("grafo teste")
+g = Grafo(dirigido=False)
+g.insert_vertice("A")
+g.insert_vertice("B")
+g.insert_vertice("C")
+g.insert_vertice("D")
+g.insert_aresta("A", "B", peso=4)
+g.insert_aresta("A", "C", peso=2)
+g.insert_aresta("B", "C", peso=5)
+g.insert_aresta("B", "D", peso=10)
+g.insert_aresta("C", "D", peso=3)
+g.mostrar_bfs("A", "D")
